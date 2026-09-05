416bc9a71ac3582a91e4dfc1ad67eec80dbb5348
hognara <hognaratest@gmail.com>
2026-09-05 20:18:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import discord
 import os
+import random
+
 from discord.ext import commands
 from discord import app_commands
 from typing import Optional
-
 
 # ============================================================
 # НАСТРОЙКИ
